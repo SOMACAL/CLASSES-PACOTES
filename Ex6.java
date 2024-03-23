b86506b0c6d29ed4dd382f40fb95fd83ad2a6989
SOMACAL <crdacostacaio@gmail.com>
2024-03-23 16:46:57 -0300

    exercicio-6

---
+++ Ex6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void executar6() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o preço de custo do produto: ");
+        double precoCusto = scanner.nextDouble();
+        
+        System.out.print("Digite o percentual de acréscimo: ");
+        double percentualAcrescimo = scanner.nextDouble();
+        
+        double precoVenda = precoCusto * (1 + percentualAcrescimo / 100);
+        
+        System.out.println("\nO preço de venda do produto é: R$" + precoVenda);
+        
+    
+       
+    }
+    
+  
+}
